a5a69b9648f3ba4dd87fa06f1f3cf3ba3e89246a
Jaswant Soni <jaswantks1812@gmail.com>
2026-01-31 09:52:04 +0530

    fix:version

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:8080', 'http://localhost:5173', 'http://localhost:3000', process.env.FRONTEND_URL].filter(Boolean),
   credentials: true,
   exposedHeaders: ['X-GST-Cookie']
 }));
